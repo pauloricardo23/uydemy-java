2ce71c975d80c6f6bfcaf1dcfe03a953e3bfa16f
Paulo Ricardo <55389102+pauloricardo23@users.noreply.github.com>
2020-08-02 18:09:21 -0300

    exercicio
    resolvido

---
+++ aula11/script/app.js
@@ -0,0 +1,6 @@
+alert("ola mundo estou de volta");
+
+let idade = parseInt ( prompt('qual o seu ano de nascimento '));
+let ano = parseInt (prompt('digite o ano atual'));
+let ida=ano-idade
+alert('a sua idade:'+ ida); /* ou alert(`a sua idade: ${ida});*/
