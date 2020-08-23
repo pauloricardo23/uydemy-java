cc3ff51c0ec3096eec1efe019ef3c4442c0d08d3
Paulo Ricardo <55389102+pauloricardo23@users.noreply.github.com>
2020-08-23 18:44:54 -0300

    apreendendo

--- aula13/script/indesfalse.js
+++ aula13/script/indesfalse.js
@@ -1,2 +1,24 @@
 alert('voce que iniciar o formulario');
-nomedeusuario=window.prompt('digite seu nome?');
+const nomedeusuario =window.prompt('digite seu nome?');
+const quantidadedeletras=
+const quantidadedeletraarepitida=
+const ultimaletra=
+cos
+
+
+window.document.body.innerHTML+=nomedeusuario;
+window.document.body.innerHTML+=quantidadedeletras
+window.document.body.innerHTML+=quantidavocais
+window.document.body.innerHTML+=
+window.document.body.innerHTML+=
+window.document.body.innerHTML+=
+window.document.body.innerHTML+=
+window.document.body.innerHTML+=
+window.document.body.innerHTML+=
+window.document.body.innerHTML+=
+window.document.body.innerHTML+=
+window.document.body.innerHTML+=
+window.document.body.innerHTML+=
+window.document.body.innerHTML+=
+
+
---
+++ aula14/script/index.js
@@ -0,0 +1,10 @@
+//Objeto Math //Numbers
+
+num1=1,887854;
+num2=5;
+//->console.log(num1.toString()+num2);
+//->console.log(num1.toString(2));
+//->console.log(num1.toFixed(2));
+//->console.log(Number.isInteger(num1));
+
+
